14ed119554bd3eee6efaed430f8867cadd0ec089
Juninho <juniorsax3@gmail.com>
2021-02-04 20:49:18 -0300

    #02:intergration
    piperDrive and bling

--- src/services/bling.js
+++ src/services/bling.js
@@ -5,6 +5,29 @@ const { response } = require('express');
 
 const bling = {
     base_url: '',
-    api_key: ''
+    api_key: '',
+
+    orders: async () => {
+        const response = await axios.get(`${bling.base_url}/pedidos/json`, {
+            params:{
+                api_key: bling.api_key
+            }
+        });
+
+        let orders = response.data;
+
+        orders = orders.retorno.pedidos.map(order => order.pedido);
+
+        return orders;
+    },
+
+    ids: orders => {
+        const ids = {};
+
+        orders.forEach(order =>{
+            ids[order.numeroOrdemCompra] = true;
+        });
+        return ids;
+    }
 
 }
--- src/services/pipeDrive.js
+++ src/services/pipeDrive.js
@@ -1,6 +1,27 @@
 const axios = require('axios');
 
 const pipeDrive = {
-    base_Url:'',
-    tokenApi:''
+    base_Url:'https://api.pipedrive.com/v1',
+    tokenApi:'659c9fddb16335e48cc67114694b52074e812e03',
+
+    list: async() => {
+        const response = await axios.get(`${pipeDrive.base_Url}/deals`, {
+            params: {
+                api_token: pipeDrive.api_token,
+                status: 'won'
+            }
+        });
+
+        const opportunities = response.data;
+
+        return opportunities.data;
+    },
+
+        removeDuplicates: (opportunities, idsOrdens) => {
+            const newOpportunities =  opportunities.filter(op => !idsOrdens.hasOwnProperty(op.id))
+
+            return newOpportunities;
+    }
 }
+
+module.exports = pipeDrive;
